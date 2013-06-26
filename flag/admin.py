fe3663ba651d63863cc812a68d5ebc8fc79f5d7e
Jeff Triplett <jeff.triplett@gmail.com>
2013-06-26 13:30:32 -0500

    Added raw_id_fields to make viewing in the admin doable.

--- flag/admin.py
+++ flag/admin.py
@@ -6,10 +6,12 @@ from .models import FlaggedContent, FlagInstance
 class InlineFlagInstance(admin.TabularInline):
     model = FlagInstance
     extra = 0
+    raw_id_fields = ['user']
 
 
 class FlaggedContentAdmin(admin.ModelAdmin):
     inlines = [InlineFlagInstance]
+    raw_id_fields = ['creator', 'moderator']
 
 
 admin.site.register(FlaggedContent, FlaggedContentAdmin)
